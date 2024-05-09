5425745d6ab5a9d7becde2e50d4a8eaec0b1d2e0
tha-orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-05-09 06:15:16 +0100

    settings updated

--- debtors/settings.py
+++ debtors/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
 ]
 
-AUTH_USER_MODEL= 'base.User'
+# AUTH_USER_MODEL= 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

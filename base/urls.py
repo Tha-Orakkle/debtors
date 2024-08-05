68dace479d1c28b4770e86af9ad8bfdeac6af872
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-08-05 01:13:18 +0100

    partitioning code

--- base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerPage, name='register'),
-]

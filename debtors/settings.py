cd697c1672da2b85dcc38e5555e69983eb6f7fc1
tha-orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-10-10 22:32:33 +0100

    update: backend middleware implemented to attach token collected from client request to the internal request made my the server to APIs

--- debtors/settings.py
+++ debtors/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'base.backends.middleware.TokenCookieToHeaderMiddleware',
 ]
 
 ROOT_URLCONF = 'debtors.urls'
